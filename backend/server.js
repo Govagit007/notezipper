50105156281d33621a8d3e8d286cb6925ec40a5d
AKKAMGARI VENU <bt19cse013@nituk.ac.in>
2023-09-26 19:42:44 +0530

    pp3

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectDB();
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://heroic-buttercream-1a9a4f.netlify.app/login"
+    "https://heroic-buttercream-1a9a4f.netlify.app"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
